edfe8a7b6214752abbcf2809f0668d5de52877fd
dai_qi_xiang <1290023174@qq.com>
2024-09-27 22:35:34 +0800

    up

--- file-handle/docs.md
+++ file-handle/docs.md
@@ -1,12 +1,24 @@
 ## Intro
 
+用于删除注释与空行
+
 ------
 
 ## Content
 
+- file_handle.py
+  1. -rm -empty       //删除空行
+  2. -rm -comment  //删除注释
+
 ------
 
 ## Use
+
+```
+python file_handle -rm -empty -comment 目标文件
+```
+
+
 
 ------
 
--- file-handle/file-handle.py
+++ file-handle/file-handle.py
@@ -37,7 +37,7 @@ def remove_comments(file_path):
             # 删除 C/C++、Java、JavaScript 的行内注释
             cleaned_line = re.sub(r'//.*$', '', cleaned_line)
             # 删除多行注释（C/C++、Java、JavaScript）
-            cleaned_line = re.sub(r'/\*.*?\*/', '', cleaned_line, flags=re.DOTALL)  #TODO:存在bug
+            cleaned_line = re.sub(r'/{\*.*?\*}/', '', cleaned_line, flags=re.DOTALL)  #TODO:存在bug
            # 删除 PHP 的行内注释
             cleaned_line = re.sub(r'//.*$', '', cleaned_line)
             cleaned_line = re.sub(r'\#.*$', '', cleaned_line) 
@@ -63,12 +63,41 @@ def remove_comments(file_path):
     except Exception as e:
         print(f"处理文件时出错: {e}")
 
+def remove_empty_comments(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            
+        cleaned_lines = []
+        for line in lines:
+            cleaned_line = re.sub(r'(?<!["\'])#.*$', '', line)
+            cleaned_line = re.sub(r'//.*$', '', cleaned_line)
+            cleaned_line = re.sub(r'/{\*.*?\*}/', '', cleaned_line, flags=re.DOTALL)  #TODO:存在bug
+            cleaned_line = re.sub(r'//.*$', '', cleaned_line)
+            cleaned_line = re.sub(r'\#.*$', '', cleaned_line) 
+            cleaned_line = re.sub(r'<!--.*?-->', '', cleaned_line, flags=re.DOTALL)
+            cleaned_line = re.sub(r'(?<!["\'])#.*$', '', cleaned_line)
+            cleaned_lines.append(cleaned_line)
+
+        non_empty_lines = [line for line in cleaned_lines if line.strip()]
+        output_dir = 'output'
+        os.makedirs(output_dir, exist_ok=True)
+        output_file_path = os.path.join(output_dir, os.path.basename(file_path))
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            file.writelines(non_empty_lines)
+        print(f"已成功删除空行与注释，并将结果保存到 {output_file_path}。")
+    except Exception as e:
+        print(f"处理文件时出错: {e}")
 
 if __name__ == "__main__":
     
-    if sys.argv[1] == '-rm' and sys.argv[2] == '-empty':
+    if sys.argv[1] == '-rm' and sys.argv[2] == '-empty' and sys.argv[3] == '-comment' or \
+       sys.argv[1] == '-rm' and sys.argv[2] == '-comment' and sys.argv[3] == '-empty':
+       remove_empty_comments(sys.argv[4])
+    elif sys.argv[1] == '-rm' and sys.argv[2] == '-empty':
         remove_empty_lines(sys.argv[3])
     elif sys.argv[1] == '-rm' and sys.argv[2] == '-comment':
         remove_comments(sys.argv[3])
+   
     else:
         print("Error")
